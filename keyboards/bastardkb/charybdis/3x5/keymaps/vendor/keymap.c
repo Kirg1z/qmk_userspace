5daa9d4f3aed71b2d5b6db0a25b0285c4dba6b93
Kirg1z <72548430+Kirg1z@users.noreply.github.com>
2026-02-20 15:36:07 +0300

    Update keymap.c

--- keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
@@ -30,19 +30,41 @@ enum charybdis_keymap_layers {
     LAYER_SYMBOLS,
 };
 
+// Названия комбо
 enum combo_events {
     SD_LCLICK,
     XC_RCLICK,
+    WE_CAPS,
+    ER_ALT_F4,
+    DF_CTRL_C,
+    CV_CTRL_V,
+    IO_WIN_E,
+    UPRIGHT_ALT_LEFT,
+    PGUPEND_ALT_UP,
 };
 
 // Последовательности клавиш
-const uint16_t PROGMEM sd_combo[] = {LCTL_T(KC_D), LALT_T(KC_S), COMBO_END};
+const uint16_t PROGMEM sd_combo[] = {LALT_T(KC_S), LCTL_T(KC_D), COMBO_END};
 const uint16_t PROGMEM xc_combo[] = {KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM we_combo[] = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM er_combo[] = {KC_E, KC_R, COMBO_END};
+const uint16_t PROGMEM df_combo[] = {LCTL_T(KC_D), KC_F, COMBO_END};
+const uint16_t PROGMEM cv_combo[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM upright_combo[] = {KC_UP, KC_RIGHT, COMBO_END};
+const uint16_t PROGMEM pgupend_combo[] = {KC_PGUP, KC_END, COMBO_END};
 
 // Маппинг
 combo_t key_combos[] = {
-    [SD_LCLICK] = COMBO(sd_combo, KC_MS_BTN1),  // Левая кнопка
-    [XC_RCLICK] = COMBO(xc_combo, KC_MS_BTN2),  // Правая кнопка
+    [SD_LCLICK] = COMBO(sd_combo, KC_MS_BTN1),       // S+D = Левая кнопка мыши
+    [XC_RCLICK] = COMBO(xc_combo, KC_MS_BTN2),       // X+C = Правая кнопка мыши
+    [WE_CAPS] = COMBO(we_combo, KC_CAPS),           // W+E = Caps Lock
+    [ER_ALT_F4] = COMBO(er_combo, LALT(KC_F4)),     // E+R = Alt+F4
+    [DF_CTRL_C] = COMBO(df_combo, LCTL(KC_C)),      // D+F = Ctrl+C
+    [CV_CTRL_V] = COMBO(cv_combo, LCTL(KC_V)),      // C+V = Ctrl+V
+    [IO_WIN_E] = COMBO(io_combo, LGUI(KC_E)),       // I+O = Win+E (Проводник)
+    [UPRIGHT_ALT_LEFT] = COMBO(upright_combo, LALT(KC_LEFT)),   // Up+Right = Alt+Left (Назад)
+    [PGUPEND_ALT_UP] = COMBO(pgupend_combo, LALT(KC_UP)),       // PgUp+End = Alt+Up (Вверх в проводнике)
 };
 // Automatically enable sniping-mode on the pointer layer.
 #define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
