660d2dbd0ff1b57708c3893a0290ebf4b172fe68
Kirg1z <72548430+Kirg1z@users.noreply.github.com>
2026-02-21 12:27:00 +0300

    Update keymap.c

--- keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
@@ -30,7 +30,6 @@ enum charybdis_keymap_layers {
     LAYER_SYMBOLS,
 };
 
-// Названия комбо
 enum combo_events {
     SD_LCLICK,
     XC_RCLICK,
@@ -41,6 +40,7 @@ enum combo_events {
     IO_WIN_E,
     UPRIGHT_ALT_LEFT,
     PGUPEND_ALT_UP,
+    UI_WIN_SPACE,
 };
 
 // Последовательности клавиш
@@ -51,20 +51,22 @@ const uint16_t PROGMEM er_combo[] = {KC_E, KC_R, COMBO_END};
 const uint16_t PROGMEM df_combo[] = {LCTL_T(KC_D), LSFT_T(KC_F), COMBO_END};
 const uint16_t PROGMEM cv_combo[] = {KC_C, KC_V, COMBO_END};
 const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM ui_combo[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM upright_combo[] = {KC_UP, KC_RIGHT, COMBO_END};
 const uint16_t PROGMEM pgupend_combo[] = {KC_PGUP, KC_END, COMBO_END};
 
 // Маппинг
 combo_t key_combos[] = {
-    [SD_LCLICK] = COMBO(sd_combo, KC_MS_BTN1),       // S+D = Левая кнопка мыши
-    [XC_RCLICK] = COMBO(xc_combo, KC_MS_BTN2),       // X+C = Правая кнопка мыши
-    [WE_CAPS] = COMBO(we_combo, KC_CAPS),           // W+E = Caps Lock
-    [ER_ALT_F4] = COMBO(er_combo, LALT(KC_F4)),     // E+R = Alt+F4
-    [DF_CTRL_C] = COMBO(df_combo, LCTL(KC_C)),      // D+F = Ctrl+C
-    [CV_CTRL_V] = COMBO(cv_combo, LCTL(KC_V)),      // C+V = Ctrl+V
-    [IO_WIN_E] = COMBO(io_combo, LGUI(KC_E)),       // I+O = Win+E (Проводник)
-    [UPRIGHT_ALT_LEFT] = COMBO(upright_combo, LALT(KC_LEFT)),   // Up+Right = Alt+Left (Назад)
-    [PGUPEND_ALT_UP] = COMBO(pgupend_combo, LALT(KC_UP)),       // PgUp+End = Alt+Up (Вверх в проводнике)
+    [SD_LCLICK] = COMBO(sd_combo, KC_MS_BTN1),
+    [XC_RCLICK] = COMBO(xc_combo, KC_MS_BTN2),
+    [WE_CAPS] = COMBO(we_combo, KC_CAPS),
+    [ER_ALT_F4] = COMBO(er_combo, LALT(KC_F4)),
+    [DF_CTRL_C] = COMBO(df_combo, LCTL(KC_C)),
+    [CV_CTRL_V] = COMBO(cv_combo, LCTL(KC_V)),
+    [IO_WIN_E] = COMBO(io_combo, LGUI(KC_E)),
+    [UPRIGHT_ALT_LEFT] = COMBO(upright_combo, LALT(KC_LEFT)),
+    [PGUPEND_ALT_UP] = COMBO(pgupend_combo, LALT(KC_UP)),
+    [UI_WIN_SPACE] = COMBO(ui_combo, LGUI(KC_SPACE)),
 };
 // Automatically enable sniping-mode on the pointer layer.
 #define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
